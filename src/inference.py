79ddad9da45021a4a5ca442afcedf1155b20a2ba
Piotr Grabysz <pgrabysz@yahoo.com>
2025-06-27 13:59:54 +0200

    feat: inference.py script

---
+++ src/inference.py
@@ -0,0 +1,42 @@
+import io
+import os
+from typing import Any
+
+import numpy as np
+import torch
+
+from src.data.augmentation import build_inference_transforms
+from src.model import UNet
+
+
+def model_fn(model_dir: str) -> torch.nn.Module:
+    model_path = os.path.join(model_dir, "best-model.ckpt")
+    model = UNet.load_from_checkpoint(model_path)
+
+    model.eval()
+    model.freeze()
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    return model
+
+
+def input_fn(request_body: Any, request_content_type: str) -> torch.Tensor:
+    if request_content_type == "application/x-npy":
+        images = np.load(io.BytesIO(request_body))
+    else:
+        raise ValueError(f"Unsupported content type: {request_content_type}")
+
+    transforms = build_inference_transforms(target_size=256)
+    image_batch = torch.tensor(
+        np.stack([transforms(image=img)["image"] for img in images], axis=0)
+    )
+    return image_batch
+
+
+def predict_fn(input_data: torch.Tensor, model: torch.nn.Module) -> torch.Tensor:
+    with torch.no_grad():
+        logits = model(input_data.to(model.device))
+        preds = torch.sigmoid(logits)
+        test_masks_prediction = (preds > 0.5).float()
+    return test_masks_prediction
